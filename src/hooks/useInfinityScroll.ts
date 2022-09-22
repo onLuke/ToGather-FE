e9fd06fe52009bddb596bccae9d7aaec0facdaf9
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-22 21:17:06 +0900

    feat:무한 스크롤 Hook 구현

---
+++ src/hooks/useInfinityScroll.ts
@@ -0,0 +1,30 @@
+import { RefObject, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { pageNumber } from '../contexts/chachingOptionAtom';
+
+const useInfinityScroll = ({ target }: { target: RefObject<HTMLElement> }) => {
+  const [page, setPage] = useRecoilState(pageNumber);
+  useEffect(() => {
+    const targetElement = target.current;
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setPage(page + 1);
+        }
+      });
+    });
+
+    if (targetElement) {
+      observer.observe(targetElement);
+    }
+
+    return () => {
+      if (targetElement) {
+        observer.unobserve(targetElement);
+      }
+    };
+  }, [target.current]);
+};
+
+export default useInfinityScroll;
