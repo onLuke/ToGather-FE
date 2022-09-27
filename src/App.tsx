75ff5d863612fdc15052f1e523980b68665361e1
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-27 19:09:11 +0900

    chore: 테스팅을 위한 임시 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainPage from './pages/MainPage';
 import ModalProvider from './contexts/ModalContext';
 import AuthRedirectPage from './pages/AuthRedirectPage';
 import ChatPage from './pages/ChatPage';
-import MyPage from './pages/MyPage';
+/* import MyPage from './pages/MyPage'; */
 import NotFoundPage from './pages/NotFoundPage';
 import HeaderNavigation from './components/Header/HeaderNavigation';
 import { useRecoilState } from 'recoil';
@@ -32,7 +32,7 @@ const App = () => {
         <HeaderNavigation />
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/mypage" element={<MyPage />} />
+          {/*           <Route path="/mypage" element={<MyPage />} /> */}
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/oauth/:social" element={<AuthRedirectPage />} />
           <Route path="/uploadStudy" element={<UploadStudy />} />
