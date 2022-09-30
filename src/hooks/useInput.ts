9e85252e6ac2362b8c9a6d03180f990d96ddf2ab
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-30 19:09:23 +0900

    feat: Editor 미입력 시 입력 요구 기능 구현

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { NeedValueAtom } from 'src/contexts/needValue';
 import S3UploadImage from './useS3UploadImage';
 
 interface Data {
@@ -12,6 +14,7 @@ interface Tech {
 
 const useInput = (initailValue: any) => {
   const [form, setForm] = useState(initailValue);
+  const [option, setOption] = useRecoilState(NeedValueAtom);
 
   const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -56,8 +59,6 @@ const useInput = (initailValue: any) => {
     const dayToString = String(date.getDate());
     const dayString = String(date.getDate()).length < 2 ? '0' + dayToString : dayToString;
 
-    console.log(dayString);
-
     const dateValue = `${date.getFullYear()}-${date.getMonth() + 1}-${dayString}`;
     setForm({ ...form, deadline: dateValue });
   };
@@ -75,6 +76,7 @@ const useInput = (initailValue: any) => {
       const filterdData = form[name].filter(
         (item: number) => item != targetAction.removedValue.value
       );
+
       setForm({ ...form, [name]: filterdData });
     } else {
       for (let item of targetValue) {
@@ -82,6 +84,40 @@ const useInput = (initailValue: any) => {
         setForm({ ...form, [name]: [...form[name], value] });
       }
     }
+    if (targetValue.length === 0) setOption({ ...option, techStackIds: true });
+    else setOption({ ...option, techStackIds: false });
+  };
+  const changeInputWithCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    const { name, value } = e.target;
+    if (e.target.files) {
+      setForm({ ...form, [name]: URL.createObjectURL(e.target.files[0]) });
+    } else {
+      setForm({ ...form, [name]: value });
+    }
+    console.log(value);
+
+    if (value !== null) {
+      setOption({ ...option, [(e.target as Element).id]: false });
+      console.log(option);
+    }
+  };
+
+  const selectChangeWithCheck = (targetValue: any, targetName: any) => {
+    const { name } = targetName;
+    const { value } = targetValue;
+    setForm({ ...form, [name]: value });
+    if (value !== null) {
+      setOption({ ...option, [targetName.name]: false });
+    }
+  };
+
+  const datePickerChangeWithCheck = (date: Date) => {
+    const dayToString = String(date.getDate());
+    const dayString = String(date.getDate()).length < 2 ? '0' + dayToString : dayToString;
+
+    const dateValue = `${date.getFullYear()}-${date.getMonth() + 1}-${dayString}`;
+    setForm({ ...form, deadline: dateValue });
   };
 
   return {
@@ -93,6 +129,9 @@ const useInput = (initailValue: any) => {
     editorChange,
     idNameToMultiSelect,
     multiSelectUpload,
+    changeInputWithCheck,
+    selectChangeWithCheck,
+    datePickerChangeWithCheck,
   };
 };
 
