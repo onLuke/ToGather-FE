f5d5120f1dd09efd4d94d0a23f41c4f403154c3d
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-30 20:07:58 +0900

    chore: 다른 페이지에서 사용하는 함수 제거, 회원 페이지에서만 사용

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { NeedValueAtom } from 'src/contexts/needValue';
 import S3UploadImage from './useS3UploadImage';
 
 interface Data {
@@ -14,7 +12,6 @@ interface Tech {
 
 const useInput = (initailValue: any) => {
   const [form, setForm] = useState(initailValue);
-  const [option, setOption] = useRecoilState(NeedValueAtom);
 
   const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -67,55 +64,6 @@ const useInput = (initailValue: any) => {
     setForm({ ...form, content: value });
   };
 
-  const multiSelectUpload = (targetValue: any, targetAction: any) => {
-    const { action, name } = targetAction;
-
-    if (action === 'clear') {
-      setForm({ ...form, [name]: [] });
-    } else if (action === 'remove-value') {
-      const filterdData = form[name].filter(
-        (item: number) => item != targetAction.removedValue.value
-      );
-
-      setForm({ ...form, [name]: filterdData });
-    } else {
-      for (let item of targetValue) {
-        let { value } = item;
-        setForm({ ...form, [name]: [...form[name], value] });
-      }
-    }
-    if (targetValue.length === 0) setOption({ ...option, techStackIds: true });
-    else setOption({ ...option, techStackIds: false });
-  };
-  const changeInputWithCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    if (e.target.files) {
-      setForm({ ...form, [name]: URL.createObjectURL(e.target.files[0]) });
-    } else {
-      setForm({ ...form, [name]: value });
-    }
-    if (value !== null) {
-      setOption({ ...option, [(e.target as Element).id]: false });
-    }
-  };
-
-  const selectChangeWithCheck = (targetValue: any, targetName: any) => {
-    const { name } = targetName;
-    const { value } = targetValue;
-    setForm({ ...form, [name]: value });
-    if (value !== null) {
-      setOption({ ...option, [targetName.name]: false });
-    }
-  };
-
-  const datePickerChangeWithCheck = (date: Date) => {
-    const dayToString = String(date.getDate());
-    const dayString = String(date.getDate()).length < 2 ? '0' + dayToString : dayToString;
-
-    const dateValue = `${date.getFullYear()}-${date.getMonth() + 1}-${dayString}`;
-    setForm({ ...form, deadline: dateValue });
-  };
-
   return {
     form,
     changeInput,
@@ -124,10 +72,6 @@ const useInput = (initailValue: any) => {
     datePickerChange,
     editorChange,
     idNameToMultiSelect,
-    multiSelectUpload,
-    changeInputWithCheck,
-    selectChangeWithCheck,
-    datePickerChangeWithCheck,
   };
 };
 
